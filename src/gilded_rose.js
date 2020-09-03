9e5503ecf9169e639f22b00688798f2056c47674
Oliver Francescatti <ofrancescatti@yahoo.com>
2020-09-03 15:42:35 -0500

    refactoring to assignment operators

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -45,39 +45,39 @@ const determineAction = (item) => {
 
 const agedBrieAction = (item) => {
   if (item.quality < 50) {
-    item.quality = item.quality + 1;
+    item.quality += 1;
   }
-  item.sell_in = item.sell_in - 1;
+  item.sell_in -= 1;
 }
 
 const backstageAction = (item) => {
   if (item.quality < 50) {
     if (item.sell_in <= 10) {
       if (item.sell_in <= 5) {
-        item.quality = item.quality + 3;
+        item.quality += 3;
       } else {
-        item.quality = item.quality + 2;
+        item.quality += 2;
       }
     } else {
-      item.quality = item.quality + 1;
+      item.quality += 1;
     }
   }
   if(item.sell_in === 0) {
-    item.quality = item.quality - item.quality;
+    item.quality = 0;
   }
-  item.sell_in = item.sell_in - 1;
+  item.sell_in -= 1;
 }
 
 const conjuredAction = (item) => {
-  item.quality = item.quality - 1;
+  item.quality -= 1;
 }
 
 const defaultAction = (item) => {
   if (item.quality > 0) {
-    item.quality = item.quality - 1;
+    item.quality -= 1;
     if(item.sell_in <= 0) {
-      item.quality = item.quality - 1;
+      item.quality -= 1;
     }
   }
-  item.sell_in = item.sell_in - 1;
+  item.sell_in -= 1;
 }
