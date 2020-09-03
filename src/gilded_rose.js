c92117104f442ccc7d01bb7a712b7ca0cce920e7
Oliver Francescatti <ofrancescatti@yahoo.com>
2020-09-03 13:12:46 -0500

    changing negative test to positive

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -26,16 +26,7 @@ const updateItem = (item) => {
 }
 
 const updateItemQuality = (item) => {
-  if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-    if (item.quality > 0) {
-      if (item.name != 'Sulfuras, Hand of Ragnaros') {
-        item.quality = item.quality - 1
-        if(item.name === 'Conjured Mana Cake') {
-          item.quality = item.quality - 1;
-        }
-      }
-    }
-  } else {
+  if (item.name === 'Aged Brie' || item.name === 'Backstage passes to a TAFKAL80ETC concert') {
     if (item.quality < 50) {
       item.quality = item.quality + 1
       if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
@@ -48,6 +39,15 @@ const updateItemQuality = (item) => {
           if (item.quality < 50) {
             item.quality = item.quality + 1
           }
+        }
+      }
+    }
+  } else {
+    if (item.quality > 0) {
+      if (item.name != 'Sulfuras, Hand of Ragnaros') {
+        item.quality = item.quality - 1
+        if(item.name === 'Conjured Mana Cake') {
+          item.quality = item.quality - 1;
         }
       }
     }
