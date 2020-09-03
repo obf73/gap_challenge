24d660ca4dd0beeb00aff535672ac2ab0d38ece3
Oliver Francescatti <ofrancescatti@yahoo.com>
2020-09-03 13:06:29 -0500

    pulling out quality updates into function

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -21,6 +21,30 @@ export const update_quality = (items) => {
 }
 
 const updateItem = (item) => {
+  updateItemQuality(item);
+  if (item.name != 'Sulfuras, Hand of Ragnaros') {
+    item.sell_in = item.sell_in - 1;
+  }
+  if (item.sell_in < 0) {
+    if (item.name != 'Aged Brie') {
+      if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+        if (item.quality > 0) {
+          if (item.name != 'Sulfuras, Hand of Ragnaros') {
+            item.quality = item.quality - 1
+          }
+        }
+      } else {
+        item.quality = item.quality - item.quality
+      }
+    } else {
+      if (item.quality < 50) {
+        item.quality = item.quality + 1
+      }
+    }
+  }
+}
+
+const updateItemQuality = (item) => {
   if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
     if (item.quality > 0) {
       if (item.name != 'Sulfuras, Hand of Ragnaros') {
@@ -44,26 +68,6 @@ const updateItem = (item) => {
             item.quality = item.quality + 1
           }
         }
-      }
-    }
-  }
-  if (item.name != 'Sulfuras, Hand of Ragnaros') {
-    item.sell_in = item.sell_in - 1;
-  }
-  if (item.sell_in < 0) {
-    if (item.name != 'Aged Brie') {
-      if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (item.quality > 0) {
-          if (item.name != 'Sulfuras, Hand of Ragnaros') {
-            item.quality = item.quality - 1
-          }
-        }
-      } else {
-        item.quality = item.quality - item.quality
-      }
-    } else {
-      if (item.quality < 50) {
-        item.quality = item.quality + 1
       }
     }
   }
