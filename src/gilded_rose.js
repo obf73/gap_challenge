03804c2824e93e7eaae69e6797523ebc33a69360
Oliver Francescatti <ofrancescatti@yahoo.com>
2020-09-03 18:50:59 -0500

    removing if statements

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -44,27 +44,19 @@ const determineAction = (item) => {
 }
 
 const agedBrieAction = (item) => {
-  if (item.quality < 50) {
-    item.quality += 1;
-  }
+  item.quality < 50 ? item.quality += 1 : item.quality;
   item.sell_in -= 1;
+}
+
+const specialIncrement = (item) => {
+  item.sell_in <= 5 ? item.quality += 3 : item.quality += 2
 }
 
 const backstageAction = (item) => {
   if (item.quality < 50) {
-    if (item.sell_in <= 10) {
-      if (item.sell_in <= 5) {
-        item.quality += 3;
-      } else {
-        item.quality += 2;
-      }
-    } else {
-      item.quality += 1;
-    }
+    item.sell_in <= 10 ? specialIncrement(item) : item.quality += 1
   }
-  if(item.sell_in === 0) {
-    item.quality = 0;
-  }
+  item.sell_in === 0 ? item.quality = 0 : item.quality
   item.sell_in -= 1;
 }
 
@@ -73,11 +65,7 @@ const conjuredAction = (item) => {
 }
 
 const defaultAction = (item) => {
-  if (item.quality > 0) {
-    item.quality -= 1;
-    if(item.sell_in <= 0) {
-      item.quality -= 1;
-    }
-  }
+  item.quality > 0 ? item.quality -= 1 : item.quality
+  item.sell_in <= 0 ? item.quality -= 1 : item.quality
   item.sell_in -= 1;
 }
