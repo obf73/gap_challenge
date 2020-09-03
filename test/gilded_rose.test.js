9eae12f4086f2247187483fdfb8ee7118f3c49a5
Oliver Francescatti <ofrancescatti@yahoo.com>
2020-09-03 12:05:03 -0500

    aged brie should decrement sell_in prop by 1

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -53,4 +53,47 @@ describe('update_quality', () => {
     })
 
   })
+
+  describe('Aged Brie', () => {
+
+    afterEach(() => {
+      testItems = []
+    })
+
+    it('should decrement sell_in prop by 1', () => {
+      const item = new Item('Aged Brie', 10, 20);
+      testItems.push(item);
+      update_quality(testItems);
+      expect(testItems[0].sell_in).toEqual(9);
+    })
+
+    // it('should decrement quality prop by 1', () => {
+    //   const item = new Item('Aged Brie', 10, 20);
+    //   testItems.push(item)
+    //   update_quality(testItems);
+    //   expect(testItems[0].quality).toEqual(19);
+    // })
+
+    // it('allow sell_in prop to decrement below 0', () => {
+    //   const item = new Item('Aged Brie', 0, 20);
+    //   testItems.push(item)
+    //   update_quality(testItems);
+    //   expect(testItems[0].sell_in).toEqual(-1);
+    // })
+
+    // it('decrement quality 2x as fast after sell_in reaches 0', () => {
+    //   const item = new Item('Aged Brie', 0, 20);
+    //   testItems.push(item)
+    //   update_quality(testItems);
+    //   expect(testItems[0].quality).toEqual(18);
+    // })
+
+    // it('never allows negative quality', () => {
+    //   const item = new Item('Aged Brie', 10, 0);
+    //   testItems.push(item)
+    //   update_quality(testItems);
+    //   expect(testItems[0].quality).toEqual(0);
+    // })
+
+  })
 })
