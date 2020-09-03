1c22812f9c7c8977a0734ce68611221d32ec6c53
Oliver Francescatti <ofrancescatti@yahoo.com>
2020-09-03 15:19:43 -0500

    refactoring actions into functions

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -5,7 +5,7 @@ function Item(name, sell_in, quality) {
 }
 
 export { Item }
-export const items = []
+export const items = [];
 
 items.push(new Item('+5 Dexterity Vest', 10, 20));
 items.push(new Item('Aged Brie', 2, 0));
@@ -29,39 +29,55 @@ const determineAction = (item) => {
   let itemName = item.name
   switch(itemName) {
     case AGED_BRIE:
-      if (item.quality < 50) {
-        item.quality = item.quality + 1
-      }
-      item.sell_in = item.sell_in - 1;
+    agedBrieAction(item);
       break;
     case BACKSTAGE:
-      if (item.quality < 50) {
-        if (item.sell_in <= 10) {
-          if (item.sell_in <= 5) {
-            item.quality = item.quality + 3
-          } else {
-            item.quality = item.quality + 2
-          }
-        } else {
-          item.quality = item.quality + 1
-        }
-      }
-      if(item.sell_in === 0) {
-        item.quality = item.quality - item.quality
-      }
-      item.sell_in = item.sell_in - 1;
+      backstageAction(item);
       break;
     case SULFURAS:
-      break
+      break;
     case CONJURED:
-      item.quality = item.quality - 1;
+      conjuredAction(item);
     default:
-      if (item.quality > 0) {
-        item.quality = item.quality - 1
-        if(item.sell_in <= 0) {
-          item.quality = item.quality - 1
-        }
-      }
-      item.sell_in = item.sell_in - 1;
+      defaultAction(item);
   }
+}
+
+const agedBrieAction = (item) => {
+  if (item.quality < 50) {
+    item.quality = item.quality + 1;
+  }
+  item.sell_in = item.sell_in - 1;
+}
+
+const backstageAction = (item) => {
+  if (item.quality < 50) {
+    if (item.sell_in <= 10) {
+      if (item.sell_in <= 5) {
+        item.quality = item.quality + 3;
+      } else {
+        item.quality = item.quality + 2;
+      }
+    } else {
+      item.quality = item.quality + 1;
+    }
+  }
+  if(item.sell_in === 0) {
+    item.quality = item.quality - item.quality;
+  }
+  item.sell_in = item.sell_in - 1;
+}
+
+const conjuredAction = (item) => {
+  item.quality = item.quality - 1;
+}
+
+const defaultAction = (item) => {
+  if (item.quality > 0) {
+    item.quality = item.quality - 1;
+    if(item.sell_in <= 0) {
+      item.quality = item.quality - 1;
+    }
+  }
+  item.sell_in = item.sell_in - 1;
 }
