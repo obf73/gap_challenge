3e5e625c22a761e9e980b66eae5e727b4f8a001b
Oliver Francescatti <ofrancescatti@yahoo.com>
2020-09-03 19:01:49 -0500

    refactoring out increments and decrements to functions

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -44,28 +44,40 @@ const determineAction = (item) => {
 }
 
 const agedBrieAction = (item) => {
-  item.quality < 50 ? item.quality += 1 : item.quality;
-  item.sell_in -= 1;
-}
-
-const specialIncrement = (item) => {
-  item.sell_in <= 5 ? item.quality += 3 : item.quality += 2
+  item.quality < 50 ? incrementQualityBy(item, 1) : item.quality;
+  decrementSellInBy(item, 1)
 }
 
 const backstageAction = (item) => {
   if (item.quality < 50) {
-    item.sell_in <= 10 ? specialIncrement(item) : item.quality += 1
+    item.sell_in <= 10 ? specialIncrement(item) : incrementQualityBy(item, 1)
   }
-  item.sell_in === 0 ? item.quality = 0 : item.quality
-  item.sell_in -= 1;
+  item.sell_in === 0 ? decrementQualityBy(item, item.quality) : item.quality
+  decrementSellInBy(item, 1)
 }
 
 const conjuredAction = (item) => {
-  item.quality -= 1;
+  decrementQualityBy(item, 1)
 }
 
 const defaultAction = (item) => {
-  item.quality > 0 ? item.quality -= 1 : item.quality
-  item.sell_in <= 0 ? item.quality -= 1 : item.quality
-  item.sell_in -= 1;
+  item.quality > 0 ? decrementQualityBy(item, 1) : item.quality
+  item.sell_in <= 0 ? decrementQualityBy(item, 1) : item.quality
+  decrementSellInBy(item, 1)
+}
+
+const specialIncrement = (item) => {
+  item.sell_in <= 5 ? incrementQualityBy(item, 3) : incrementQualityBy(item, 2)
+}
+
+const incrementQualityBy = (item, increment) => {
+  item.quality += increment
+}
+
+const decrementQualityBy = (item, increment) => {
+  item.quality -= increment
+}
+
+const decrementSellInBy = (item, increment) => {
+  item.sell_in -= increment
 }
